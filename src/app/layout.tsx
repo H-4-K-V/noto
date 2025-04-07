a87f16474cad10e0da614b7b99165ee7d9ecb54a
h4kv <vitor.sannctorum@hotmail.com>
2025-04-06 23:15:54 -0300

    feat: configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Work_Sans } from "next/font/google";
 import "./globals.css";
 
 const font = Work_Sans({
-  variable: "--font-work-sans",
   subsets: ["latin"],
 });
 
@@ -19,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${font.variable} antialiased`}>{children}</body>
+      <body className={`${font.className} antialiased`}>{children}</body>
     </html>
   );
 }
